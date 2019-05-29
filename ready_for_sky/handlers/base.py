0c4bb60f6d3b1878b478a806441a02a3d2e2fe6d
Yuri Apollov <apollovy@gmail.com>
2019-05-29 22:34:30 +0300

    Truly async and non-blocking mysql read and rsa key generation.

--- ready_for_sky/handlers/base.py
+++ ready_for_sky/handlers/base.py
@@ -1,5 +1,7 @@
+import asyncio
 import os
 import typing
+from concurrent import futures
 
 # noinspection PyPackageRequirements
 import MySQLdb
@@ -9,21 +11,31 @@ from Crypto.PublicKey import RSA
 
 DB_SERVER_HOST = os.environ['DB_SERVER_HOST']
 
+# this will be different for every process
+CONNECTION: MySQLdb.Connection = None
+
+
+# noinspection PyUnusedLocal
+def initializer(*args, **kwargs):
+    global CONNECTION
+    CONNECTION = MySQLdb.connect(
+        host=DB_SERVER_HOST, passwd='example', db='ready_for_sky',
+        user='root',
+    )
+
 
 # noinspection PyAbstractClass
 class MainHandler(tornado.web.RequestHandler):
-    connection: MySQLdb.Connection
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.connection = MySQLdb.connect(
-            host=DB_SERVER_HOST, passwd='example', db='ready_for_sky',
-            user='root',
-        )
+    executor = futures.ProcessPoolExecutor(initializer=initializer)
 
     async def get(self, user_name):
-        key = await read_or_create_public_key(self.connection, user_name)
+        key = await self.read_or_create_public_key(user_name)
         self.write(key)
+
+    async def read_or_create_public_key(self, user_name: str):
+        return await asyncio.wrap_future(
+            self.executor.submit(read_or_create_public_key, user_name)
+        )
 
 
 # noinspection PyPep8Naming
@@ -41,18 +53,20 @@ def generate_RSA() -> typing.Tuple[bytes, bytes]:
     return private_key, public_key
 
 
-async def read_or_create_public_key(connection, user_name: str) -> bytes:
-    maybe_key = await read_public_key(connection, user_name)
+def read_or_create_public_key(user_name: str) -> bytes:
+    # noinspection PyTypeChecker
+    maybe_key = read_public_key(CONNECTION, user_name)
 
     if maybe_key is None:
-        key = await create_public_key(connection, user_name)
+        # noinspection PyTypeChecker
+        key = create_public_key(CONNECTION, user_name)
     else:
         key = maybe_key
 
     return key
 
 
-async def create_public_key(connection, user_name: str) -> bytes:
+def create_public_key(connection, user_name: str) -> bytes:
     cursor = connection.cursor()
     _, public_key = generate_RSA()
     sql = insert_sql()
@@ -62,7 +76,7 @@ async def create_public_key(connection, user_name: str) -> bytes:
     return public_key
 
 
-async def read_public_key(connection, user_name: str) -> typing.Optional[bytes]:
+def read_public_key(connection, user_name: str) -> typing.Optional[bytes]:
     cursor = connection.cursor()
     sql = select_sql()
     cursor.execute(sql, (user_name,))
--- tests/test_.py
+++ tests/test_.py
@@ -54,22 +54,23 @@ def user_name(request):
 
 
 @pytest.fixture
-async def public_key_created(mysql_connection, user_name):
-    result = await base.create_public_key(mysql_connection, user_name)
+def public_key_created(mysql_connection, user_name):
+    result = base.create_public_key(mysql_connection, user_name)
 
     return result
 
 
 @pytest.fixture
-async def public_key_read(mysql_connection, user_name):
-    result = await base.read_public_key(mysql_connection, user_name)
+def public_key_read(mysql_connection, user_name):
+    result = base.read_public_key(mysql_connection, user_name)
 
     return result
 
 
 @pytest.fixture
-async def public_key_read_or_created(mysql_connection, user_name):
-    key = await base.read_or_create_public_key(mysql_connection, user_name)
+def public_key_read_or_created(mysql_connection, user_name):
+    base.CONNECTION = mysql_connection
+    key = base.read_or_create_public_key(user_name)
 
     return key
 
@@ -82,16 +83,14 @@ def test_public_key_creates_successfully(table_cleared, public_key_created):
     assert public_key_created
 
 
-@pytest.mark.asyncio
-async def test_public_key_reads(
+def test_public_key_reads(
         table_cleared, generate_rsa_patched,
         mysql_connection, public_key_created, public_key_read,
 ):
     assert public_key_read == PUBLIC_KEY
 
 
-@pytest.mark.asyncio
-async def test_public_key_reads_or_creates(
+def test_public_key_reads_or_creates(
         table_cleared, generate_rsa_patched,
         mysql_connection, public_key_read_or_created,
 ):
